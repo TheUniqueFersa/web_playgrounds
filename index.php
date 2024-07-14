22606a1a3cd06b64ad747bad79fd5a3a52dbfc95
TheUniqueFersa <holafersa@gmail.com>
2024-07-14 13:55:25 -0600

    Estructura inicial de la cabecera

--- index.php
+++ index.php
@@ -7,20 +7,28 @@
         <link rel="stylesheet" href="./assets/styles/css/styles.css">
     </head>
     <body>
-        <h1>Cambio 1</h1>
+        <!-- <h1>Cambio 1</h1>
 
         <h2>Cambio 2</h2>
         <h2>Cambio 3 :o</h2>
         <nav>Hola</nav>
-        <!-- Ejercicio de Emmet: -->
         <div>
-            <h4>1</h4>
+            <h4>1</h4>Que
         </div>
         <div>   
             <h4>2</h4>
         </div>
         <div>
             <h4>3</h4>
+        </div> -->
+        <div id="container">
+            <div id="cabecera">
+                Cabecera
+            </div>
+            <div id="post">
+                <h1>Este es el título atractivo e interesante del post</h1>
+                <p>Y este es el párrafo de inicio donde vamos a explicar las cosas increíbles que se pueden hacer con ramas</p>
+            </div>
         </div>
     </body>
 </html>
